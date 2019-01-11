d7ad6254a9ebe64a2e6a2d54f1bafd37a76cc74c
treyliebscher <wcliebsc@gmail.com>
2019-01-11 12:19:30 -0500

    refactored to rely on Player component

--- src/components/StatusBox.jsx
+++ src/components/StatusBox.jsx
@@ -1,34 +1,12 @@
 import React, {component} from 'react';
+import Chip from './Chip';
+import Player from './Player';
 
 class StatusBox extends React.Component {
     render(){
 
         const players = this.props.game.players.map((player, index) => {
-            // TODO consider creating a component for chips, logic between diff value chips could become complex
-            return  <li className="player-list-item" key={index}>
-                        <div>{player.name}</div>
-                        <div className="chip-holder">
-                            <div className="chip white">${this.props.game.chipValues.white.value}</div>
-                            <div>{player.chips.white}</div>
-                        </div>
-                        <div className="chip-holder">
-                            <div className="chip green">${this.props.game.chipValues.green.value}</div>
-                            <div>{player.chips.green}</div>
-                        </div>
-                        <div className="chip-holder">
-                            <div className="chip red">${this.props.game.chipValues.red.value}</div>
-                            <div>{player.chips.red}</div>
-                        </div>
-                        <div className="chip-holder">
-                            <div className="chip blue">${this.props.game.chipValues.blue.value}</div>
-                            <div>{player.chips.blue}</div>
-                        </div>
-                        <div className="chip-holder">
-                            <div className="chip black">${this.props.game.chipValues.black.value}</div>
-                            <div>{player.chips.black}</div>
-                        </div>
-                    </li>
-
+            return <Player player={player} chipValues={this.props.game.chipValues} key={index}/>
         });
 
         const playerSection = () => {
