a62d066380599fab3a7dc0338df06a8a28ff8a7a
treyliebscher <wcliebsc@gmail.com>
2019-01-11 00:48:16 -0500

    added unique keys for players + cleanup

--- src/pages/SetupGame.jsx
+++ src/pages/SetupGame.jsx
@@ -5,15 +5,16 @@ import PlayerForm from '../components/PlayerForm';
 import store from '../store';
 import './SetupGame.css'
 import StatusBox from '../components/StatusBox';
+import ChipValueForm from '../components/ChipValueForm';
 
 // for testing - TODO remove when no longer required
-store.dispatch(chipValue({
-    white: {value: 5, quantity: 50}, 
-    red: {value: 10, quantity: 25}, 
-    green: {value: 15, quantity: 20}, 
-    blue: {value: 20, quantity: 10}, 
-    black: {value: 25, quantity: 5}
-}))
+// store.dispatch(chipValue({
+//     white: {value: 5, quantity: 50}, 
+//     red: {value: 10, quantity: 25}, 
+//     green: {value: 15, quantity: 20}, 
+//     blue: {value: 20, quantity: 10}, 
+//     black: {value: 25, quantity: 5}
+// }))
 //
 
 class SetupGame extends Component {
@@ -26,21 +27,21 @@ class SetupGame extends Component {
     render(){
 
         // TODO refactor playerStatus into its own component
-        const playerStatus = this.props.game.players.map((player) => {
+        const playerStatus = this.props.game.players.map((player, index) => {
             let dealer;
             let smallBlind;
             let bigBlind;
             if(player.status === 'dealer'){
                 dealer = player.name;
-                return <div>Dealer - {dealer}</div>
+                return <div key={index}>Dealer - {dealer}</div>
             }
             else if (player.status === 'smallBlind'){
                 smallBlind = player.name;
-                return <div>Small Blind - {smallBlind}</div>
+                return <div key={index}>Small Blind - {smallBlind}</div>
             }
             else if (player.status === 'bigBlind'){
                 bigBlind = player.name;
-                return <div>Big Blind - {bigBlind}</div>
+                return <div  key={index}>Big Blind - {bigBlind}</div>
             }
         })
 
@@ -54,6 +55,7 @@ class SetupGame extends Component {
                 <button className="test-button" onClick={e => this.props.dispatch(handOver())}>Shift dealer</button>
                 <button className="test-button" onClick={e => {this.props.dispatch(distChips())}}>Dist chips</button>
                 <StatusBox game={currentGame} />
+                <ChipValueForm />
             </div>
         )
     }
