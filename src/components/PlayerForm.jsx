0f5f68d6026fd45fc28a23abd44148e01d681e80
treyliebscher <wcliebsc@gmail.com>
2019-01-10 23:20:55 -0500

    added a form component for entering players to a game

---
+++ src/components/PlayerForm.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addPlayer } from '../actions/gameActions';
+
+export class PlayerForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        
+        const playerName = this.textInput.value.trim();
+
+        const newPlayer = {
+            name: playerName,
+            chips: {
+                white: 0,
+                green: 0,
+                red: 0,
+                blue: 0,
+                black: 0
+            },
+            status: null
+        }
+        this.props.dispatch(addPlayer(newPlayer))
+        this.textInput.value = '';
+    }
+
+    render() {
+
+        return (
+            <form className="card add-form" onSubmit={this.onSubmit}>
+                <input type="text" ref={input => this.textInput = input} />
+                <button>Add</button>
+            </form>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    game: state.game
+})
+
+export default connect(mapStateToProps)(PlayerForm)
