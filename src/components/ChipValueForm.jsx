f93432bd15e687bb9b11403f4537798d777a0a47
treyliebscher <wcliebsc@gmail.com>
2019-01-12 12:47:43 -0500

    added chipvalues to local state for better visual feedback when entering value

--- src/components/ChipValueForm.jsx
+++ src/components/ChipValueForm.jsx
@@ -7,6 +7,14 @@ export class ChipValueForm extends React.Component {
     constructor(props) {
         super(props);
         this.onSubmit = this.onSubmit.bind(this);
+        this.setValue = this.setValue.bind(this);
+        this.state = {
+            whiteVal: null,
+            greenVal: null,
+            redVal: null,
+            blueVal: null,
+            blackVal: null
+        }
     }
 
     onSubmit(event) {
@@ -45,6 +53,11 @@ export class ChipValueForm extends React.Component {
         this.blackCount.value = '';
     }
 
+    setValue(e){
+        console.log(e.target.id);
+        this.setState({[e.target.id]: e.target.value});
+    }
+
 
 
     render() {
@@ -52,38 +65,38 @@ export class ChipValueForm extends React.Component {
         return (
             <form className="chipValue-form" onSubmit={this.onSubmit}>
                 
-                <Chip chipValue={this.props.game.chipValues.white.value} chipColor="white"/>
+                <Chip chipValue={this.state.whiteVal} chipColor="white"/>
                 <label htmlFor="whiteVal">Value</label>
-                <input id="whiteVal" type="number" ref={input => this.whiteVal = input} />
+                <input id="whiteVal" type="number" ref={input => this.whiteVal = input} onChange={ this.setValue }/>
                 
                 <label htmlFor="whiteCount">Quantity</label>
-                <input id="whiteCount" type="number" ref={input => this.whiteCount = input} />
+                <input id="whiteCount" type="number" ref={input => this.whiteCount = input}/>
                 
-                <Chip chipValue={this.props.game.chipValues.green.value} chipColor="green"/>
+                <Chip chipValue={this.state.greenVal} chipColor="green"/>
                 <label htmlFor="blueVal">Value</label>
-                <input id="greenVal" type="text" ref={input => this.greenVal = input} />
+                <input id="greenVal" type="text" ref={input => this.greenVal = input} onChange={ this.setValue }/>
                 
 
                 <label htmlFor="greenCount">Quantity</label>
                 <input id="greenCount" type="text" ref={input => this.greenCount = input} />
                 
-                <Chip chipValue={this.props.game.chipValues.red.value} chipColor="red"/>
+                <Chip chipValue={this.state.redVal} chipColor="red"/>
                 <label htmlFor="redVal">Value</label>
-                <input id="redVal" type="text" ref={input => this.redVal = input} />
+                <input id="redVal" type="text" ref={input => this.redVal = input} onChange={ this.setValue }/>
                 
                 <label htmlFor="redCount">Quantity</label>
                 <input id="redCount" type="text" ref={input => this.redCount = input} />
                 
-                <Chip chipValue={this.props.game.chipValues.blue.value} chipColor="blue"/>
+                <Chip chipValue={this.state.blueVal} chipColor="blue"/>
                 <label htmlFor="blueVal">Value</label>
-                <input id="blueVal" type="text" ref={input => this.blueVal = input} />
+                <input id="blueVal" type="text" ref={input => this.blueVal = input} onChange={ this.setValue }/>
                 
                 <label htmlFor="blueCount">Quantity</label>
                 <input id="blueCount" type="text" ref={input => this.blueCount = input} />
                 
-                <Chip chipValue={this.props.game.chipValues.black.value} chipColor="black"/>
+                <Chip chipValue={this.state.blackVal} chipColor="black"/>
                 <label htmlFor="blackVal">Value</label>
-                <input id="blackVal" type="text" ref={input => this.blackVal = input} />
+                <input id="blackVal" type="text" ref={input => this.blackVal = input} onChange={ this.setValue }/>
                 
                 <label htmlFor="blackCount">Quantity</label>
                 <input id="blackCount" type="text" ref={input => this.blackCount = input} />
