0d2af8e85114122d4f475dc466bce8937e6d69e7
treyliebscher <wcliebsc@gmail.com>
2019-01-12 13:15:58 -0500

    added route for the presentational component SetChips

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, Switch } from 'react-router';
 
 import PocketPot from './pages/PocketPot';
 import SetupGame from './pages/SetupGame';
+import SetChips from './pages/SetChips';
 
 export default (
     <Switch>
         <Route exact path="/" component={PocketPot} />
         <Route exact path="/new-game" component={SetupGame} />
+        <Route exact path="/set-chips" component={SetChips} />
     </Switch>
 );
