c865b71cfd37d202282a7652af70df5716683e7f
treyliebscher <wcliebsc@gmail.com>
2019-01-15 13:36:36 -0500

    added BetForm to Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Chip from './Chip';
 import './Player.css'
 
+import BetForm from './BetForm';
+
 export class Player extends React.Component {
 
     render(){
@@ -50,6 +52,7 @@ export class Player extends React.Component {
                 <Chip chipColor="red" chipValue={this.props.chipValues.red.value} chipQuantity={this.props.player.chips.red}/>
                 <Chip chipColor="blue" chipValue={this.props.chipValues.blue.value} chipQuantity={this.props.player.chips.blue}/>
                 <Chip chipColor="black" chipValue={this.props.chipValues.black.value} chipQuantity={this.props.player.chips.black}/>
+                <BetForm />
             </li>
         )
     }
