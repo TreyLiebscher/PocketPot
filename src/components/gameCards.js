7c54bddcc309b01ac49e16bfc286cb70c47bf372
treyliebscher <wcliebsc@gmail.com>
2019-01-15 23:43:24 -0500

    created a simple store for a standard 52 card deck

---
+++ src/components/gameCards.js
@@ -0,0 +1,321 @@
+export const GAME_CARDS = [
+
+    {
+        name: '2',
+        value: 2,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: '3',
+        value: 3,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: '4',
+        value: 4,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: '5',
+        value: 5,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: '6',
+        value: 6,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: '7',
+        value: 7,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: '8',
+        value: 8,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: '9',
+        value: 9,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: '10',
+        value: 10,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: 'Jack',
+        value: 10,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: 'Queen',
+        value: 10,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: 'King',
+        value: 10,
+        wild: false,
+        suit: 'hearts'
+    },
+    {
+        name: 'Ace',
+        value: 11,
+        wild: false,
+        suit: 'hearts'
+    },
+
+
+    {
+        name: '2',
+        value: 2,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: '3',
+        value: 3,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: '4',
+        value: 4,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: '5',
+        value: 5,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: '6',
+        value: 6,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: '7',
+        value: 7,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: '8',
+        value: 8,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: '9',
+        value: 9,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: '10',
+        value: 10,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: 'Jack',
+        value: 10,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: 'Queen',
+        value: 10,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: 'King',
+        value: 10,
+        wild: false,
+        suit: 'diamonds'
+    },
+    {
+        name: 'Ace',
+        value: 11,
+        wild: false,
+        suit: 'diamonds'
+    },
+
+
+    {
+        name: '2',
+        value: 2,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: '3',
+        value: 3,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: '4',
+        value: 4,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: '5',
+        value: 5,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: '6',
+        value: 6,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: '7',
+        value: 7,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: '8',
+        value: 8,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: '9',
+        value: 9,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: '10',
+        value: 10,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: 'Jack',
+        value: 10,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: 'Queen',
+        value: 10,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: 'King',
+        value: 10,
+        wild: false,
+        suit: 'clubs'
+    },
+    {
+        name: 'Ace',
+        value: 11,
+        wild: false,
+        suit: 'clubs'
+    },
+
+
+    {
+        name: '2',
+        value: 2,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: '3',
+        value: 3,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: '4',
+        value: 4,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: '5',
+        value: 5,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: '6',
+        value: 6,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: '7',
+        value: 7,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: '8',
+        value: 8,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: '9',
+        value: 9,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: '10',
+        value: 10,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: 'Jack',
+        value: 10,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: 'Queen',
+        value: 10,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: 'King',
+        value: 10,
+        wild: false,
+        suit: 'spades'
+    },
+    {
+        name: 'Ace',
+        value: 11,
+        wild: false,
+        suit: 'spades'
+    }
+]
