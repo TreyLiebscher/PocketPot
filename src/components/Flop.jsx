ac931c6163c63f3bf886095a486bee8402d9a1f1
treyliebscher <wcliebsc@gmail.com>
2019-01-16 01:56:01 -0500

    fixed issue with repeating cards + added some visuals (WIP)

--- src/components/Flop.jsx
+++ src/components/Flop.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { GAME_CARDS } from './gameCards';
 // TODO import presentational component to sit inside of Flop
 
@@ -7,6 +8,8 @@ export class Flop extends React.Component {
         super(props);
         this.showFlop = this.showFlop.bind(this);
         this.state = {
+            cardsDealt: false,
+            playerCards: null,
             pos1: {
                 value: null,
                 hidden: true
@@ -30,111 +33,124 @@ export class Flop extends React.Component {
         }
     }
 
-    drawCard() {
-
-        return Math.floor(Math.random() * Math.floor(51));
-    }
-
-    validateCard(array, item){
-        if(array.length === 0){
-            return item;
-        }
-
-        let card;
-
-        for(let i = 0; i < array.length; i++){
-            if(array[i] == item){
-                card = this.drawCard();
-            } else {
-                card = item;
-            }
-        }
-
-        return card;
+    drawCard(deck) {
+        return Math.floor(Math.random() * Math.floor(deck));
     }
 
     generateCard(number) {
         let returnArray = [];
         let deck = GAME_CARDS.slice();
+        let deckSize = 52;
         let testArray = [];
         for (let i = 0; i < number; i++) {
-            const position2 = this.drawCard();
-            const position = this.validateCard(testArray, position2);
-            console.log(position);
+            const position = this.drawCard(deckSize);
             const card = deck.slice(position, position + 1);
-            const removeCard = () => {deck.splice(position, 1);};
-            const drawCard = new Promise(function(resolve, reject) {
-                setTimeout(function() {
-                  resolve(returnArray.push(card));
-                }, 100);
-            });
-
-            drawCard.then(() => {
-                removeCard();
-            });
-
-            testArray.push(position)
+            const removeCard = () => { deck.splice(position, 1); };
             returnArray.push(card);
-
-            console.log('kiwi deck is %s', deck.length)
-            // if(i === number){
-            //     deck = GAME_CARDS;
-            // }
+            testArray.push(position);
+            removeCard();
+            deckSize = deckSize - 1;
         }
-
-        console.log(testArray)
-
         return returnArray;
     }
 
     showFlop() {
-        console.log(this.props.players.length)
-        // const cardsArray = this.generateCard(17);
-        const getCards = this.generateCard(17);
+
+        const playerLength = this.props.players.length;
+        const cardsToDeal = (playerLength * 2) + 5;
+
+        const getCards = this.generateCard(cardsToDeal);
         const dealCards = new Promise(function (resolve, reject) {
             setTimeout(function () {
                 resolve(getCards);
             }, 100);
         });
-        console.log(GAME_CARDS.length)
 
         dealCards.then((cards) => {
-            const cardsArray = cards.flat()
-            console.log('cards are', cardsArray)
-            return cardsArray;
-        })
+            const cardsArray = cards.flat();
+            const playerCards = cardsArray.slice(5);
+            this.setState({
+                cardsDealt: true,
+                playerCards: playerCards, 
+                pos1: {
+                    value: cardsArray[0],
+                    hidden: false
+                },
+                pos2: {
+                    value: cardsArray[1],
+                    hidden: false
+                },
+                pos3: {
+                    value: cardsArray[2],
+                    hidden: false
+                },
+                pos4: {
+                    value: cardsArray[3],
+                    hidden: true
+                },
+                pos5: {
+                    value: cardsArray[4],
+                    hidden: true
+                }
+            })
 
-        // console.log('kiwi, flop is being shown', cardsArray);
-
-
-
-
-
-        // this.setState({
-        //     pos1: {
-        //         value: cardsArray[0],
-        //         hidden: false
-        //     },
-        //     pos2: {
-        //         value: cardsArray[1],
-        //         hidden: false
-        //     },
-        //     pos3: {
-        //         value: cardsArray[2],
-        //         hidden: false
-        //     }
-        // })
+        });
     }
 
     render() {
+
+        const flop = () => {
+            if (this.state.cardsDealt === true) {
+                return  <div style={{display: 'flex', margin: '1em'}}>
+                            <div style={{display: 'flex', margin: '1em'}}>
+                                <div style={{fontSize: '50px'}}>{this.state.pos1.value.name}</div>
+                                <div>
+                                    <img style={{maxHeight: '50px', maxWidth: '50px'}} src={this.state.pos1.value.image}></img>
+                                </div>
+                            </div>
+
+                            <div style={{display: 'flex', margin: '1em'}}>
+                                <div style={{fontSize: '50px'}}>{this.state.pos2.value.name}</div>
+                                <div>
+                                    <img style={{maxHeight: '50px', maxWidth: '50px'}} src={this.state.pos2.value.image}></img>
+                                </div>
+                            </div>
+                            
+                            <div style={{display: 'flex', margin: '1em'}}>
+                                <div style={{fontSize: '50px'}}>{this.state.pos3.value.name}</div>
+                                <div>
+                                    <img style={{maxHeight: '50px', maxWidth: '50px'}} src={this.state.pos3.value.image}></img>
+                                </div>
+                            </div>
+
+                            <div style={{display: 'flex', margin: '1em'}}>
+                                <div style={{fontSize: '50px'}}>{this.state.pos4.value.name}</div>
+                                <div>
+                                    <img style={{maxHeight: '50px', maxWidth: '50px'}} src={this.state.pos4.value.image}></img>
+                                </div>
+                            </div>
+
+                            <div style={{display: 'flex', margin: '1em'}}>
+                                <div style={{fontSize: '50px'}}>{this.state.pos5.value.name}</div>
+                                <div>
+                                    <img style={{maxHeight: '50px', maxWidth: '50px'}} src={this.state.pos5.value.image}></img>
+                                </div>
+                            </div>
+                        </div>
+            }
+        }
+
         return (
             <div>
                 <button onClick={this.showFlop}>FLOP TEST</button>
-                <button onClick={this.drawCard}>COUNT TEST</button>
-
+                {flop()}
             </div>
         )
     }
 }
 
-export default Flop;
+const mapStateToProps = state => ({
+    game: state.game
+})
+
+export default connect(mapStateToProps)(Flop)
