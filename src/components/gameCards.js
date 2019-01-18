e3038a9131e6ec32b974d54ef736f7bd607be719
treyliebscher <wcliebsc@gmail.com>
2019-01-18 14:54:18 -0500

    changed image for hearts

--- src/components/gameCards.js
+++ src/components/gameCards.js
@@ -5,91 +5,91 @@ export const GAME_CARDS = [
         value: 2,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: '3',
         value: 3,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: '4',
         value: 4,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: '5',
         value: 5,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: '6',
         value: 6,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: '7',
         value: 7,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: '8',
         value: 8,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: '9',
         value: 9,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: '10',
         value: 10,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: 'J',
         value: 10,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: 'Q',
         value: 10,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: 'K',
         value: 10,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
     {
         name: 'A',
         value: 11,
         wild: false,
         suit: 'hearts',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5b9uo4QZINshr08z92UKMGD0FjRbQFC0z0Bsv7zmSfQxt7p0OBQ'
+        image: 'https://cdn1.iconfinder.com/data/icons/love-icons/512/love-heart-512.png'
     },
 
 
